18c1b371fe01e8dfc0da6f49ffa54ccfa7172fea
flavioov <oliveira.v653@gmail.com>
2020-03-21 23:33:31 +0000

    my_main dir modification

--- my_main.sh
+++ my_main.sh
@@ -4,19 +4,19 @@
 echo "
 Installing all programs (python, chrome, calibre, git, vlc, etc.)
 "
-sh /home/flavio/my_tools/programs/install.sh
+sh ~/my_tools/programs/install.sh
 
 # ssh configuration
 echo "
 Copying ssh configurations and keys
 "
-sh /home/flavio/my_tools/ssh_config/install.sh
+sh ~/my_tools/ssh_config/install.sh
 
 #Nord VPN install and configuration
 echo "
 Installing and configuring Nordvpn IKEV2
 "
-sh /home/flavio/my_tools/nordvpn/install.sh 
+sh ~/my_tools/nordvpn/install.sh 
 
 echo "
 All Done !! 
