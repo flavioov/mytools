665926f88644757a2b45d42ed5e29718a903d532
Flávio Oliveira <oliveira.v653@gmail.com>
2020-04-01 17:43:44 -0300

    windows: fonts issue

--- bashrc/install.sh
+++ bashrc/install.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-<<<<<<< HEAD
-=======
-cat ~/my_tools/bashrc/bashrc_config >> ~/.bashrc
->>>>>>> 4ecb02eefaab0711409ceb0c71d7b846528da991
 cat ~/my_tools/bashrc/bashrc_config >> ~/.zshrc
 sh ~/my_tools/bashrc/update_n_clean.sh
