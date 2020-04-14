2145a7d3912682894332e13ba456d4a96e3351fb
Flávio de Oliveira <oliveira.v653@gmail.com>
2020-04-14 11:43:38 -0300

    custom terminal modif

--- tools/custom_terminal/zsh_isntall.sh
+++ tools/custom_terminal/zsh_isntall.sh
@@ -3,7 +3,7 @@ sudo apt-get install zsh
 
 chsh -s $(which zsh)
 echo $SHELL
-sudo reboot
+#sudo reboot
 
 #oh my zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
