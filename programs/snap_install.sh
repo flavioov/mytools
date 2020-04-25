559b5ae037bb1d89108091da8dc3be63fa3f1cb2
Fávio de Oliveira <oliveira.v653@gmail.com>
2020-04-24 23:25:46 -0300

    added programs to snap install

--- programs/snap_install.sh
+++ programs/snap_install.sh
@@ -3,4 +3,6 @@
 sudo snap install pycharm-professional --classic
 sudo snap install intellij-idea-ultimate --classic
 sudo snap install spotify
-
+sudo snap install whatsdesk
+sudo snap install telegram-desktop
+sudo snap install gnome-tweak-tool
